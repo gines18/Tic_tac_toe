0f5102ddc08f1b8610761a8f635fa70e3df51abb
gines18 <ruczkowski.m@gmail.com>
2023-04-29 11:46:38 +0100

    create winning conditions

--- script.js
+++ script.js
@@ -7,5 +7,28 @@ const announcer = document.querySelector('.announcer');
 let board = ['', '', '', '', '', '', '', '', ''];
 let currentPlayer = 'X';
 let isGameActive = true;
+
+const PLAYERX_WON = 'PLAYERX_WON';
+const PLAYERY_WON = 'PLAYERY_WON';
+const TIE = 'TIE';
+
+/*
+Indexes within the board
+[1][2][3]
+[4][5][6]
+[7][8][9]
+*/
+
+const winningConditions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+
 resetButton.addEventListener('click', resetBoard);
 });
